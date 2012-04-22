ad08f152e563eb169311678122d5f5eecaea5e4a
SonYa Eick <when@issonyabusy.com>
2012-04-22 15:26:59 -0400

    first commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+print "Hello Again"
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "This is my extra credit line!"
+# print "Matt is a male human."
+
+# Turns out hash tags make python ignore lines. ( 02/23/12: Or in other words, lets you add comments.)
