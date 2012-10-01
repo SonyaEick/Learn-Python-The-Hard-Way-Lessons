1b266a9288a5289366b291e19d73b381aa784c76
SonYaEick <when@issonyabusy.com>
2012-09-30 20:21:45 -0700

    Simple Blackjack Game

---
+++ blackjack.py
@@ -0,0 +1,31 @@
+from random import randint
+
+cards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+while True:
+   player = []
+   player.append
+
+
+computer_hand = randint(1,11) + randint(1,11)
+
+
+class Blackjack(object):
+
+   def bust(self):
+      starting_hand = "%d%d" % (randint(1,11), randint(1,11))
+
+      while starting_hand < 22:
+         return "Hit or Stay?"
+
+         action = raw_input("> ")
+
+         if action == "hit":
+            print starting_hand + (randint(1,11)
+            return "Hit or Stay?"
+
+         if action == "stay":
+            print starting_hand
+
+   if user_hand < computer_hand or user_hand == computer_hand
+      print "Dealer wins." 
